f57f803f1fe94d284d31fc91c444ec0308e575ec
Jef Mathiot <jeff.mathiot@gmail.com>
2013-12-21 18:18:58 +0100

    Refactored local shell.

--- lib/electric_sheeps/shell/local_shell.rb
+++ lib/electric_sheeps/shell/local_shell.rb
@@ -1,6 +1,37 @@
+require 'session'
+require 'shellwords'
+
 module ElectricSheeps
     module Shell
-        class LocalShell < Sheep
+        class LocalShell
+            def initialize(logger)
+                @logger = logger
+            end
+
+            def open!
+                return self if opened?
+                @logger.info "Starting a local shell session"
+                @session = ::Session::Sh.new
+                self
+            end
+
+            def close!
+                @session = nil
+                self
+            end
+
+            def exec(cmd)
+                return unless opened?
+                @session.execute(cmd) do |out, err|
+                    @logger.info out.chomp unless out.nil?
+                    @logger.error err.chomp unless err.nil?
+                end
+                @session.exit_status
+            end
+
+            def opened?
+                !@session.nil?
+            end
 
         end
     end
--- spec/shell/local_shell_spec.rb
+++ spec/shell/local_shell_spec.rb
@@ -2,8 +2,70 @@ require 'spec_helper'
 
 describe ElectricSheeps::Shell::LocalShell do
 
-    it 'should fail' do
-        false.must_equal true
+    before do
+        @shell_helper = ElectricSheeps::Shell::LocalShell
+        @logger = mock()
     end
 
+
+    describe "with a session" do
+
+        before do
+            @logger.expects(:info).with("Starting a local shell session")
+            @shell = @shell_helper.new( @logger )
+            @shell.open!
+        end
+
+        it "should have open a shell session" do
+            @shell.opened?.must_equal true
+            @shell.instance_variable_get(:@session).must_be_instance_of( ::Session::Sh )
+        end
+
+        it 'should output stdout to logger' do
+            @logger.expects(:info).with('Hello World')
+            @logger.expects(:error).never
+            @shell.exec 'echo "Hello World"'
+        end
+
+        it 'should output stderr to logger' do
+            @logger.expects(:error).with('Goodbye Cruel World')
+            @logger.expects(:info).never
+            @shell.exec 'echo "Goodbye Cruel World" >&2'
+        end
+
+        it 'should output both stdout and stderr to logger' do
+            @logger.expects(:info).with('Hello World')
+            @logger.expects(:error).with('Goodbye Cruel World')
+            @shell.exec 'echo "Hello World" ; echo "Goodbye Cruel World" >&2'
+        end
+
+        it 'should keep state when multiple calls' do
+            @logger.expects(:info).with('/')
+            @shell.exec 'cd /'
+            @shell.exec 'pwd'
+        end
+
+        it 'should close' do
+            @shell.close!.opened?.must_equal false
+        end
+
+        it 'should not try to reopen' do
+            @shell.open!
+        end
+
+        describe 'on returning status' do
+            it 'should succeed' do
+                @logger.expects(:info).with('Hello World')
+                @logger.expects(:error).never
+                @shell.exec('echo "Hello World"').must_equal 0
+            end
+
+            it 'should fail gracefully' do
+                @logger.expects(:info).never
+                @logger.expects(:error).at_least_once
+                @shell.exec('ls --wtf').must_equal 2
+            end
+        end
+
+    end
 end
