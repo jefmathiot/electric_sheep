e861b8f4094c3d263a8cf92ba519deefc8e3d857
Jef Mathiot <jeff.mathiot@gmail.com>
2013-12-29 17:43:35 +0100

    Added an agents file to require agents.

---
+++ lib/electric_sheeps/agents.rb
@@ -0,0 +1,8 @@
+require 'electric_sheeps/agents/agent'
+require 'electric_sheeps/agents/register'
+require 'electric_sheeps/agents/database'
+
+module ElectricSheeps
+    module Agents
+    end
+end
