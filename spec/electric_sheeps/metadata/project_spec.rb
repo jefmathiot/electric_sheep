1ff29b06d71464fa87b1cf7dfc39fcee9485c7e0
Jef Mathiot <jeff.mathiot@gmail.com>
2013-12-31 14:52:28 +0100

    Added a queue to project metadata.

--- spec/electric_sheeps/metadata/project_spec.rb
+++ spec/electric_sheeps/metadata/project_spec.rb
@@ -6,11 +6,24 @@ describe ElectricSheeps::Metadata::Project do
         subject.new(description: 'A description').description.must_equal 'A description'
     end
 
-    it 'should add shells' do
-        
-    end
+    describe 'with steps added' do
+        before do
+            @project = subject.new(description: 'A description')
+            @shell = @project.add(ElectricSheeps::Metadata::Shell.new)
+            @transport = @project.add(ElectricSheeps::Metadata::Transport.new())
+        end
 
-    it 'should add transports' do
+        it 'counts steps' do
+            @project.size.must_equal 2
+            @project.remaining.must_equal 2
+        end
+
+        it 'retains steps order' do
+            @project.next!.must_equal @shell
+            @project.remaining.must_equal 1
+            @project.next!.must_equal @transport
+            @project.remaining.must_equal 0
+        end
 
     end
 end
