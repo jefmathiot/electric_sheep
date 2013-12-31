8debf3bd7076dc73c7d2d401860743245dcf3f3b
Jef Mathiot <jeff.mathiot@gmail.com>
2013-12-31 15:06:04 +0100

    Pulled-up steps queue to a Queueable module.

--- lib/electric_sheeps/metadata/project.rb
+++ lib/electric_sheeps/metadata/project.rb
@@ -1,33 +1,10 @@
 module ElectricSheeps
     module Metadata
         class Project
+            include Queueable
 
             def initialize
-                @steps = []
                 reset!
-            end
-
-            def add(step)
-                @steps << step
-                step
-            end
-
-            def size
-                @steps.size
-            end
-
-            def remaining
-                [@steps.size - @current, 0].max
-            end
-
-            def next!
-                @current += 1
-                @steps[@current - 1]
-            end
-
-            private
-            def reset!
-                @current = 0
             end
 
             include Options
---
+++ lib/electric_sheeps/metadata/queueable.rb
@@ -0,0 +1,30 @@
+module ElectricSheeps
+    module Metadata
+        module Queueable
+
+            def add(item)
+                @items << item
+                item
+            end
+
+            def size
+                @items.size
+            end
+
+            def remaining
+                [@items.size - @current, 0].max
+            end
+
+            def next!
+                @current += 1
+                @items[@current - 1]
+            end
+
+            def reset!
+                @items = []
+                @current = 0
+            end
+
+        end
+    end
+end
