6fc227b38ee3bc02bd418f9356c3e6c40ca60660
Jef Mathiot <jeff.mathiot@gmail.com>
2014-01-01 18:05:48 +0100

    Config now wraps hosts and a queue of projects.

--- lib/electric_sheeps.rb
+++ lib/electric_sheeps.rb
@@ -4,6 +4,7 @@ require 'electric_sheeps/queue'
 require 'electric_sheeps/metadata'
 require 'electric_sheeps/resources'
 require 'electric_sheeps/agents'
+require 'electric_sheeps/config'
 require 'electric_sheeps/shell/local_shell'
 require 'electric_sheeps/shell/remote_shell'
 
---
+++ lib/electric_sheeps/config.rb
@@ -0,0 +1,13 @@
+module ElectricSheeps
+    class Config
+        include Queue
+
+        attr_reader :hosts
+
+        def initialize
+            @hosts = Metadata::Hosts.new
+            reset!
+        end
+
+    end
+end
