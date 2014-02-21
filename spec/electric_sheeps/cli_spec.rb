165150c093ae4f508ef6d20bd5ca844cb6ac0530
Jef Mathiot <jeff.mathiot@gmail.com>
2014-02-21 17:27:08 +0100

    Added spec for the CLI.

---
+++ spec/electric_sheeps/cli_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'electric_sheeps/cli'
+
+describe ElectricSheeps::CLI do
+
+    describe 'work' do
+
+        describe 'when everything goes well' do
+
+            before do
+                @config, @logger = mock, mock
+                ElectricSheeps::Log::ConsoleLogger.expects(:new).with(kind_of(IO), kind_of(IO)).
+                    returns(@logger)
+                ElectricSheeps::Runner.expects(:new).
+                    with(all_of(
+                        has_entry(config: @config),
+                        has_entry(logger: @logger)
+                    )).returns(mock(run!: true))
+            end
+
+            it 'gets the job done' do
+                ElectricSheeps::Sheepfile::Evaluator.expects(:new).with('Sheepfile').
+                    returns(mock(evaluate: @config))
+                subject.new.work
+            end
+
+            it 'overrides default config option' do
+                ElectricSheeps::Sheepfile::Evaluator.expects(:new).with('Lambfile').
+                    returns(mock(evaluate: @config))
+                subject.new([], config: 'Lambfile').work
+            end
+
+        end
+
+        it 'raises a Thor error if something went wrong' do
+            ElectricSheeps::Sheepfile::Evaluator.expects(:new).with('Sheepfile').
+                raises(RuntimeError.new)
+            -> { subject.new.work }.must_raise Thor::Error
+        end
+
+    end
+
+end
