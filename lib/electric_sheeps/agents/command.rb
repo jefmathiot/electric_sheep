f8ca7241d3cc7b6e0cd53694c40bdb9126b59976
Eric Hartmann <hartmann.eric@gmail.com>
2014-03-28 19:27:11 +0100

    Add method_missing in order to access resources

--- lib/electric_sheeps/agents/command.rb
+++ lib/electric_sheeps/agents/command.rb
@@ -10,8 +10,16 @@ module ElectricSheeps
         @logger = options[:logger]
         @shell = options[:shell]
         @work_dir = options[:work_dir]
+        @resources = options[:resources]
       end
 
+      def method_missing(method, *args, &block)
+        if @resources.has_key?(method)
+          @resources[method]
+        else
+          super
+        end
+      end
     end
   end
 end
