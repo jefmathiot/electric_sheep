e7de81173c5bc8c3facd68531573927b5cf91581
Jef Mathiot <jeff.mathiot@gmail.com>
2014-03-30 16:05:40 +0200

    Fixed S3Cmd spec.

--- spec/electric_sheeps/commands/s3/s3cmd_spec.rb
+++ spec/electric_sheeps/commands/s3/s3cmd_spec.rb
@@ -6,16 +6,19 @@ describe ElectricSheeps::Commands::S3::S3cmd do
     ElectricSheeps::Commands::Register.command('s3cmd').must_equal subject
   end
 
-  it 's3cmd should have s3_bucket and file variables' do
-    s3cmd = subject.new(nil, nil, nil, {
-      :file => ElectricSheeps::Resources::File.new(:filename => 'somefile.txt'),
-      :s3_bucket => ElectricSheeps::Resources::S3Bucket.new({:url => 'some_url', :access_key => 'some_access_key', :secret_key => 'some_secret_key'})
-    })
+  it 'expects s3bucket and file resources' do
+    s3cmd = subject.new('s3-step', nil, nil, nil, nil,
+      file: ElectricSheeps::Resources::File.new(path: 'somefile.txt'),
+      s3_bucket: ElectricSheeps::Resources::S3Bucket.new(
+        url: 'some_url',
+        access_key: 'some_access_key',
+        secret_key: 'some_secret_key'
+      )
+    )
 
     s3cmd.s3_bucket.url.must_equal 'some_url'
     s3cmd.s3_bucket.access_key.must_equal 'some_access_key'
     s3cmd.s3_bucket.secret_key.must_equal 'some_secret_key'
-    s3cmd.file.filename.must_equal 'somefile.txt'
-    s3cmd.check_prerequisites
+    s3cmd.file.path.must_equal 'somefile.txt'
   end
 end
