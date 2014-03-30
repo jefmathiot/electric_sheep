dc61df8e511efd3e81e77bd0e5f7c0b479a4c499
Jef Mathiot <jeff.mathiot@gmail.com>
2014-03-30 15:50:59 +0200

    Projects now allows storage of command products.

--- lib/electric_sheeps/metadata/project.rb
+++ lib/electric_sheeps/metadata/project.rb
@@ -4,10 +4,19 @@ module ElectricSheeps
       include Queue
       include Metered
 
-      attr_accessor :description
+      attr_accessor :description, :products
 
       def initialize
         reset!
+        @products = {}.with_indifferent_access
+      end
+
+      def store_product(step_id, resource)
+        @products[step_id] = resource
+      end
+
+      def product_of(step_id)
+        @products[step_id]
       end
 
       include Options
--- spec/electric_sheeps/metadata/project_spec.rb
+++ spec/electric_sheeps/metadata/project_spec.rb
@@ -14,10 +14,19 @@ describe ElectricSheeps::Metadata::Project do
   it "initializes the project's id" do
     project = subject.new(id: 'some-project')
     project.id.must_equal 'some-project'
+    project.products.keys.size.must_equal 0
   end
 
   it 'defines a description accessor' do
     expects_accessor(:description)
+  end
+
+  it 'stores and retrieves products' do
+    project = subject.new
+    project.store_product('step', resource = mock)
+    ['step', :step].each do |step|
+      project.product_of(step).must_equal resource
+    end
   end
 
 end
