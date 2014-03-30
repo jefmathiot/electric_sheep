40041c2c93eeae80caf2d7795955d3f1ec82eb01
Jef Mathiot <jeff.mathiot@gmail.com>
2014-03-30 17:47:50 +0200

    Named helper methods support the extension option.

--- lib/electric_sheeps/helpers/named.rb
+++ lib/electric_sheeps/helpers/named.rb
@@ -4,8 +4,9 @@ module ElectricSheeps
       include ShellSafe
       include Timestamps
 
-      def with_named_dir(*args, &block)
-        with_named_path *args, &block
+      def with_named_dir(base_dir, name, options={}, &block)
+        options.delete(:extension)
+        with_named_path base_dir, name, options, &block
       end
 
       def with_named_file(*args, &block)
@@ -15,6 +16,7 @@ module ElectricSheeps
       protected
       def with_named_path(base_dir, name, options={}, &block)
         name = "#{name}-#{timestamp}" if options[:timestamp]
+        name = "#{name}.#{options[:extension]}" if options.has_key?(:extension)
         File.join(base_dir, shell_safe(name)).tap do |resource|
           yield resource if block_given?
         end
--- spec/electric_sheeps/helpers/named_spec.rb
+++ spec/electric_sheeps/helpers/named_spec.rb
@@ -7,39 +7,51 @@ describe ElectricSheeps::Helpers::Named do
     include ElectricSheeps::Helpers::Named
   end
 
+  before do
+    @named = NamedKlazz.new
+  end
+
   %w(dir file).each do |type|
 
-  describe "creating #{type} names" do
+    describe "creating #{type} names" do
 
-    before do
-      @named = NamedKlazz.new
+      it 'creates a simple name' do
+        @named.send("with_named_#{type}", '/tmp', type).must_equal "/tmp/#{type}"
+      end
+
+      it 'escapes the name' do
+        @named.send("with_named_#{type}", '/tmp', "\"#{type}").must_equal "/tmp/\\\"#{type}"
+      end
+
+      it 'appends a timestamp' do
+        Timecop.travel(Time.utc(2014, 1, 2, 3, 2, 1)) do
+          @named.send("with_named_#{type}", '/tmp', type, timestamp: true).must_equal "/tmp/#{type}-20140102-030201"
+        end
+      end
+
+      it 'yields the provided block' do
+        yielded = nil
+        result = @named.send("with_named_#{type}", '/tmp', type) do |file|
+          yielded = file
+          "some-arbitrary-expression"
+        end
+        result.must_equal "/tmp/#{type}"
+        yielded.must_equal result
+      end
+
     end
 
-    it 'creates a simple name' do
-      @named.send("with_named_#{type}", '/tmp', type).must_equal "/tmp/#{type}"
+    it 'appends the extension to file name' do
+      @named.with_named_file('/tmp', 'some-file', extension: 'rb').
+        must_equal '/tmp/some-file.rb'
     end
-
-    it 'escapes the name' do
-      @named.send("with_named_#{type}", '/tmp', "\"#{type}").must_equal "/tmp/\\\"#{type}"
-    end
-
-    it 'appends a timestamp' do
+    
+    it 'appends both the timestamp and extension to file name' do
       Timecop.travel(Time.utc(2014, 1, 2, 3, 2, 1)) do
-        @named.send("with_named_#{type}", '/tmp', type, timestamp: true).must_equal "/tmp/#{type}-20140102-030201"
+        @named.with_named_file('/tmp', 'some-file', timestamp: true, extension: 'rb').
+        must_equal '/tmp/some-file-20140102-030201.rb'
       end
     end
-
-    it 'yields the provided block' do
-      yielded = nil
-      result = @named.send("with_named_#{type}", '/tmp', type) do |file|
-        yielded = file
-        "some-arbitrary-expression"
-      end
-      result.must_equal "/tmp/#{type}"
-      yielded.must_equal result
-    end
-
-  end
 
   end
 
