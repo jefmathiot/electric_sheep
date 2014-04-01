aaa821b70d385dc22a54fd4ec173eee7d7a3f2af
Jef Mathiot <jeff.mathiot@gmail.com>
2014-04-01 14:21:03 +0200

    Provisioner for acceptance container now creates a MySQL database. Provisioner for acceptance container now creates a MongoDB database.

--- acceptance/setup.sh
+++ acceptance/setup.sh
@@ -2,3 +2,16 @@
 
 service mysql restart
 service mongodb restart
+
+mysql --user=root --password=pseudorandom <<EOF
+  DROP DATABASE IF EXISTS controldb;
+  CREATE DATABASE controldb;
+  GRANT USAGE ON *.* TO 'operator'@'localhost' IDENTIFIED BY 'pseudorandom';
+  GRANT ALL PRIVILEGES ON controldb.* to 'operator'@'localhost';
+EOF
+
+mongo <<EOF
+  use controldb;
+  db.dropDatabase();
+  db.data.save({pi: 3.14});
+EOF
