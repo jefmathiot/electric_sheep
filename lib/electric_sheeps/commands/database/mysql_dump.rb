2f7e05afe694cc2f4a110105828a9501e70c6448
Jef Mathiot <jeff.mathiot@gmail.com>
2014-04-01 16:56:17 +0200

    Added an empty perform method to the MySQL dump command.

--- lib/electric_sheeps/commands/database/mysql_dump.rb
+++ lib/electric_sheeps/commands/database/mysql_dump.rb
@@ -8,6 +8,10 @@ module ElectricSheeps
 
         resource :database, kind_of: Resources::Database
 
+        def perform
+          logger.info "Creating a dump of the \"#{database.name}\" MySQL database"
+        end
+
       end
     end
   end
