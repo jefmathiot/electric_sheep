dbb9f464069b13549e6897ccdfa5f35433e4e34a
Jef Mathiot <jeff.mathiot@gmail.com>
2014-04-23 12:11:30 +0200

    Create an helper module Resourceful allowing shells to build resources.

---
+++ lib/electric_sheep/shell/resourceful.rb
@@ -0,0 +1,22 @@
+module ElectricSheep
+  module Shell
+    module Resourceful
+      
+      def directory_resource(options)
+        Resources::Directory.new(resource_options(options))
+      end
+      
+      def file_resource(options)
+        Resources::File.new(resource_options(options))
+      end
+
+      def resource_options(options)
+        if local?
+          options.merge(host: Metadata::Localhost.new)
+        else
+          options.merge(host: @host)
+        end
+      end
+    end
+  end
+end
