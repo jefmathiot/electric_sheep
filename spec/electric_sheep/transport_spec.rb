414a2e845c8c903f6e2e2492a10aa3b937fd32e7
Jef Mathiot <jeff.mathiot@gmail.com>
2014-09-22 12:54:54 +0200

    Add missing spec for transport execution

---
+++ spec/electric_sheep/transport_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe ElectricSheep::Transport do
+  TransportKlazz = Class.new do
+    include ElectricSheep::Transport
+
+    attr_reader :project, :logger, :metadata, :hosts, :done
+
+    def do
+      @done = true
+    end
+  end
+
+  describe TransportKlazz do
+    it 'performs using transport type from metadata' do
+      transport=subject.new(project=mock, logger=mock, metadata=mock, hosts=mock)
+      metadata.expects(:type).returns(:do)
+      transport.perform
+
+      transport.project.must_equal project
+      transport.logger.must_equal logger
+      transport.metadata.must_equal metadata
+      transport.hosts.must_equal hosts
+      transport.done.must_equal true
+    end
+  end
+end
