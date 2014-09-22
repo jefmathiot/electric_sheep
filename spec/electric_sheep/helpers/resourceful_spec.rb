12ac644d231b1ec14cc917de037c6ce54c2f7b5c
Jef Mathiot <jeff.mathiot@gmail.com>
2014-09-22 09:57:20 +0200

    Fix resourceful spec

--- spec/electric_sheep/helpers/resourceful_spec.rb
+++ spec/electric_sheep/helpers/resourceful_spec.rb
@@ -13,22 +13,17 @@ describe ElectricSheep::Helpers::Resourceful do
 
   describe ResourcefulKlazz do
 
-    def assert_resource_created(type, klazz, local_expected)
-      ResourcefulKlazz.any_instance.stubs(:local?).returns local_expected
-      resource=subject.new.send "#{type}_resource", path: '/some/path'
+    def assert_resource_created(type, klazz)
+      resource=subject.new.send "#{type}_resource", host=mock, '/some/path'
       resource.must_be_instance_of klazz
       resource.path.must_equal '/some/path'
-      resource.host.send local_expected ? :must_be : :wont_be, :local?
+      resource.host.must_equal host
     end
 
     def self.describe_resource_creation(type, klazz)
       describe "creating a #{type} resource" do
-        it 'merges the options and defaults to localhost' do
-          assert_resource_created type, klazz, true
-        end
-        
-        it 'merges the options and keep the provided host if remote' do
-          assert_resource_created type, klazz, false
+        it 'merges the options' do
+          assert_resource_created type, klazz
         end
       end
     end
