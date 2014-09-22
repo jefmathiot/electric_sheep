c8e886a2d5a76d00764a4b472b8328d958a5b9c8
Jef Mathiot <jeff.mathiot@gmail.com>
2014-09-22 11:47:14 +0200

    Fix directories spec

--- spec/electric_sheep/helpers/directories_spec.rb
+++ spec/electric_sheep/helpers/directories_spec.rb
@@ -2,22 +2,32 @@ require 'spec_helper'
 
 describe ElectricSheep::Helpers::Directories do
 
-  before do
-    @subject = subject.new(@shell = mock)
-    @seq = sequence('shell')
+  let(:seq) do
+    sequence('shell')
   end
 
-  it 'creates the project directory' do
-    project = mock
-    project.stubs(:id).returns('My Project')
-
-    @shell.expects(:exec).in_sequence(@seq).
-      with('echo ${ELECTRIC_SHEEP_HOME-"$HOME/.electric_sheep"}').
-      returns({out: '/es/home'})
-    @shell.expects(:exec).in_sequence(@seq).
-      with('mkdir -p /es/home/my\\ project ; chmod 0700 /es/home/my\\ project')
-
-    @subject.mk_project_dir!(project)
+  let(:host) do
+    mock
   end
 
+  describe 'returning working directory' do
+    it 'defaults to home' do
+      host.expects(:working_directory).returns(nil)
+      subject.working_directory(host).must_equal '$HOME/.electric_sheep'
+    end
+
+    it 'uses the host working directory if any' do
+      host.expects(:working_directory).returns('/tmp')
+      subject.working_directory(host).must_equal '/tmp'
+    end
+  end
+
+  describe 'returning project directory' do
+    it 'uses the project id' do
+      host.expects(:working_directory).returns('/tmp')
+      project=mock
+      project.expects(:id).returns('SOME PROJECT')
+      subject.project_directory(host, project).must_equal "/tmp/some\\ project"
+    end
+  end
 end
