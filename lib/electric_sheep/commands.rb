37ec6c3a31f915361409254d1c043e81b3d42271
Jef Mathiot <jeff.mathiot@gmail.com>
2014-09-22 11:39:44 +0200

    Remove deprecated S3cmd command

--- lib/electric_sheep/commands.rb
+++ lib/electric_sheep/commands.rb
@@ -1,6 +1,5 @@
 require 'electric_sheep/commands/compression'
 require 'electric_sheep/commands/database'
-require 'electric_sheep/commands/s3'
 
 module ElectricSheep
   module Commands
--- lib/electric_sheep/commands/s3.rb
+++
@@ -1,8 +0,0 @@
-require 'electric_sheep/commands/s3/s3cmd'
-
-module ElectricSheep
-  module Commands
-    module S3
-    end
-  end
-end
