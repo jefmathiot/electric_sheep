4e7f60e6c260008c3bcfb1ca1fe3c34e6f42db8c
Jef Mathiot <jeff.mathiot@gmail.com>
2014-10-10 18:02:51 +0200

    Improve readability of TarGz

--- lib/electric_sheep/commands/compression/tar_gz.rb
+++ lib/electric_sheep/commands/compression/tar_gz.rb
@@ -14,14 +14,18 @@ module ElectricSheep
           input_path=shell.expand_path(input.path)
           done!(
             file_resource(extension: '.tar.gz').tap do |archive|
-              shell.exec "cd #{File.dirname(input_path)}; " +
-                "tar -cvzf #{shell.expand_path(archive.path)} "+
-                "#{File.basename(input_path)} &> /dev/null"
+              shell.exec cmd(input_path, archive)
               shell.exec "rm -f #{input_path}" if option(:delete_source)
             end
           )
         end
 
+        private
+        def cmd(input_path, archive)
+          "cd #{File.dirname(input_path)}; " +
+            "tar -cvzf #{shell.expand_path(archive.path)} "+
+            "#{File.basename(input_path)} &> /dev/null"
+        end
       end
     end
   end
