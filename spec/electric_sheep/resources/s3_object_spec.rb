5d1e43b0e4f5c3313657c899fa5735e8dbe4c325
Jef Mathiot <jeff.mathiot@gmail.com>
2014-12-03 14:45:24 +0100

    Improve S3 object coverage

--- spec/electric_sheep/resources/s3_object_spec.rb
+++ spec/electric_sheep/resources/s3_object_spec.rb
@@ -9,4 +9,16 @@ describe ElectricSheep::Resources::S3Object do
     defines_options :directory, :bucket
     requires :bucket
   }
+
+  it 'is remote only' do
+    subject.new.local?.must_equal false
+  end
+
+  it 'normalize its path' do
+    resource=subject.new(path: 'path/to/file.ext')
+    resource.extension.must_equal '.ext'
+    resource.basename.must_equal 'file'
+    resource.parent.must_equal 'path/to'
+  end
+
 end
