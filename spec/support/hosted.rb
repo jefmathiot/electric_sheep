e7eeea617c3d1eebde9087218b8833cc31db10b6
Jef Mathiot <jeff.mathiot@gmail.com>
2014-12-23 17:30:24 +0100

    Add missing support module

---
+++ spec/support/hosted.rb
@@ -0,0 +1,18 @@
+module Support
+  module Hosted
+    extend ActiveSupport::Concern
+
+    included do
+      it do
+        defines_options :host
+        requires :host
+      end
+
+      it 'delegates conversion to location to host' do
+        subject.new(host: mock(to_location: location=mock)).to_location.
+          must_equal location
+      end
+    end
+
+  end
+end
