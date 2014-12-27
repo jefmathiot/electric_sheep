3aac3354c7f9b273e9d7b17d27da70a798884c44
Jef Mathiot <jeff.mathiot@gmail.com>
2014-12-27 18:09:33 +0100

    Report execution stack in a format adapted to notifiers

--- lib/electric_sheep/metadata/pipe.rb
+++ lib/electric_sheep/metadata/pipe.rb
@@ -6,27 +6,42 @@ module ElectricSheep
 
       attr_reader :input, :start_location
 
-      def pipelined(resource, &block)
+      def pipelined(resource, parent=nil, &block)
+        @report=parent.report unless parent.nil?
         init(resource)
         monitored do
           iterate do |item|
-            done!( item, yield(item, last_product), item.execution_time )
+            begin
+              result=yield(item, last_output)
+            ensure
+              if result.is_a?(Array)
+                done!( item,  *result)
+              else
+                done!( item,  result, result)
+              end
+            end
           end
         end
       end
 
-      def last_product
-        execs.size > 0 && execs.last.product || input
+      def last_output
+        @last_output || input
       end
 
-      def execs
-        @execs ||= []
+      def last_product
+        @last_product
+      end
+
+      def report
+        @report ||= Report.new(input, start_location)
       end
 
       protected
 
-      def done!(metadata, product, execution_time)
-        execs << ExecTrail.new(metadata, product, execution_time)
+      def done!(metadata, product, output)
+        @last_output = output
+        @last_product = product
+        report.step metadata, product, output
       end
 
       def init(resource)
@@ -34,9 +49,80 @@ module ElectricSheep
         @start_location=resource.to_location
       end
 
-      ExecTrail=Struct.new(:metadata, :product, :execution_time)
-
       Location=Struct.new(:id, :location, :type)
+
+      class Report
+
+        def initialize(resource, location)
+          step = SimpleStep.new(:resource, resource)
+          @stack = [WrapperStep.new(location, 'mainstream', [ step ])]
+        end
+
+        def stack
+          @stack
+        end
+
+        def step( metadata, product, output )
+          return unless metadata.is_a?(Metadata::Agent)
+          if @reference
+            append_resource(@reference, true)
+            @reference=nil
+          end
+          if metadata.is_a?(Metadata::Transport)
+            @stack << SimpleStep.new(:transport, metadata)
+          else
+            @stack.last.steps << SimpleStep.new(:command, metadata)
+          end
+          if product != output
+            if product.to_location != output.to_location
+              change_location(product.to_location, 'fork')
+            end
+            append_resource(product)
+            @reference=output
+          else
+            append_resource(output, true)
+          end
+        end
+
+        private
+
+        def append_resource(resource, change_location=false)
+          if resource
+            change_location(resource.to_location) if change_location
+            @stack.last.steps << SimpleStep.new(:resource, resource)
+          end
+        end
+
+        def change_location(location, branch='mainstream')
+          last=@stack.last
+          unless last.respond_to?(:location) && last.location==location
+            @stack << WrapperStep.new(location, branch)
+          end
+        end
+
+        SimpleStep=Struct.new(:type, :payload)
+
+        class WrapperStep
+
+          attr_reader :location, :branch, :steps
+
+          def initialize(location, branch, steps=[])
+            @location=location
+            @branch=branch
+            @steps=steps
+          end
+
+          def resources
+            steps.select{|step| step.type==:resource}
+          end
+
+          def agents
+            steps.select{|step| [:command, :transport].include?(step.type) }
+          end
+
+        end
+
+      end
 
     end
 
--- spec/electric_sheep/metadata/pipe_spec.rb
+++ spec/electric_sheep/metadata/pipe_spec.rb
@@ -25,47 +25,212 @@ describe ElectricSheep::Metadata::Pipe do
         pipe.start_location.must_equal start_location
       end
 
+      it 'creates a report using the input and start location' do
+        ElectricSheep::Metadata::Pipe::Report.expects(:new).
+          with(resource, start_location).returns(report=mock)
+        pipe.pipelined(resource)
+        pipe.report.must_equal report
+      end
+
+      it 'uses the parent report if provided' do
+        pipe.pipelined(resource, mock(report: parent_report=mock))
+        pipe.report.must_equal parent_report
+      end
+
       describe 'iterating through its queue' do
 
-        let(:products){ [] }
-        let(:resources){ [] }
+        [:products, :outputs, :resources].each do |array|
+          let(array){ [] }
+        end
 
         before do
           [item1, item2].each do |item|
             pipe.add item
-          end
-          time=0
-          pipe.pipelined(resource) do |item, resource|
             products << mock
+            outputs << mock
+          end
+        end
+
+        def execute_pipeline
+          pipe.pipelined(resource) do |item, resource|
             resources << resource
-            item.stubs(:execution_time).returns(time+=1)
-            products.last
+            index = item==item1 ? 0 : 1
+            [products[index], outputs[index]]
           end
         end
 
         it 'retains the original input and start location' do
+          execute_pipeline
           pipe.input.must_equal resource
           pipe.start_location.must_equal start_location
         end
 
-        it 'creates an exec trail for each step' do
-          pipe.execs.size.must_equal 2
-        end
-
         it 'yields the previous product to each block' do
-          resources.must_equal [resource, products.first]
+          execute_pipeline
+          resources.must_equal [resource, outputs.first]
         end
 
-        it 'keeps metadata, product and execution time of each step' do
-          pipe.execs.first.tap do |trail|
-            trail.metadata.must_equal item1
-            trail.product.must_equal products.first
-            trail.execution_time.must_equal 1
+        it 'reports' do
+          [item1, item2].each_with_index do |item, i|
+            pipe.report.expects(:step).
+              with(item, products[i], outputs[i])
           end
+          execute_pipeline
         end
 
         it 'returns the last product' do
+          execute_pipeline
           pipe.last_product.must_equal products.last
+        end
+
+      end
+
+    end
+
+  end
+
+  describe ElectricSheep::Metadata::Pipe::Report do
+
+    [:resource, :start_location, :alt_location, :product].each do |m|
+      let(m){ mock }
+    end
+
+    let(:report){ subject.new(resource, start_location) }
+
+    it 'appends the input resource and start location to the stack' do
+      report.stack.first.location.must_equal start_location
+      report.stack.first.steps.first.tap do |step|
+        step.type.must_equal :resource
+        step.payload.must_equal resource
+      end
+    end
+
+    it 'ignores non-agent metadata' do
+      report.step(Object.new, nil, nil)
+      report.stack.size.must_equal 1
+      report.stack.first.steps.size.must_equal 1
+    end
+
+
+    def assert_step(step, type, payload)
+      step.type.must_equal type
+      step.payload.must_equal payload
+    end
+
+    let(:command){ ElectricSheep::Metadata::Command.new }
+
+    describe 'appending a command to the stack' do
+
+
+      def step
+        report.step(command, product, product)
+      end
+
+      it 'keeps the same location' do
+        product.stubs(:to_location).returns(start_location)
+        step
+        report.stack.size.must_equal 1
+        assert_step report.stack.first.steps[1], :command, command
+        assert_step report.stack.first.steps.last, :resource, product
+      end
+
+      # TODO Should we exclude that weird case ? It's not supposed to happen, as
+      # commands do not change resources locations
+      it 'changes the location on the mainstream branch' do
+        product.stubs(:to_location).returns(alt_location)
+        step
+        report.stack.size.must_equal 2
+        report.stack.last.location.must_equal alt_location
+        report.stack.last.branch.must_equal 'mainstream'
+        assert_step report.stack.first.steps.last, :command, command
+        assert_step report.stack.last.steps.first, :resource, product
+      end
+
+    end
+
+    describe 'appending a transport to the stack' do
+
+      let(:transport){ ElectricSheep::Metadata::Transport.new }
+
+      def step(output)
+        report.step(transport, product, output)
+      end
+
+      before do
+        product.stubs(:to_location).returns(alt_location)
+      end
+
+      def assert_transport_steps(location, branch)
+        assert_step report.stack[1], :transport, transport
+        report.stack.last.location.must_equal location
+        report.stack.last.branch.must_equal branch
+        report.stack.last.steps.size.must_equal 1
+        assert_step report.stack.last.steps.first, :resource, product
+      end
+
+      it 'changes the location on the mainstream branch' do
+        step(product)
+        report.stack.size.must_equal 3
+        assert_transport_steps alt_location, 'mainstream'
+      end
+
+      describe 'forking' do
+
+        let(:output){ mock.tap{ |m| m.stubs(:to_location).returns(mock) } }
+
+        it 'doesnt restart' do
+          step(output)
+          report.stack.size.must_equal 3
+          assert_transport_steps alt_location, 'fork'
+        end
+
+        it 'restarts to the reference resource' do
+          next_resource=mock(to_location: output.to_location)
+          step(output)
+          report.step(command, next_resource, next_resource)
+          report.stack.size.must_equal 4
+          report.stack.last.location.must_equal output.to_location
+          assert_step report.stack.last.steps.first, :resource, output
+          assert_step report.stack.last.steps[1], :command, command
+          assert_step report.stack.last.steps.last, :resource, next_resource
+        end
+
+      end
+
+    end
+
+
+    describe ElectricSheep::Metadata::Pipe::Report::WrapperStep do
+
+      it 'initializes with empty steps' do
+        subject.new(start_location, 'branch').tap do |step|
+          step.location.must_equal start_location
+          step.branch.must_equal 'branch'
+          step.steps.must_equal []
+        end
+      end
+
+      it 'uses provided steps if any' do
+        subject.new(nil, nil, steps=[mock, mock]).
+          steps.must_equal steps
+      end
+
+      describe 'with steps' do
+
+        [:command, :transport, :resource].each do |m|
+          let(m){ ElectricSheep::Metadata::Pipe::Report::SimpleStep.new(m) }
+        end
+
+        let(:wrapper){
+          subject.new(nil, nil, [command, transport, resource])
+        }
+
+        it 'filters resources' do
+          wrapper.resources.must_equal [resource]
+        end
+
+        it 'filters agents' do
+          wrapper.agents.must_equal [command, transport]
         end
 
       end
