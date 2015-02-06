5264fb4382f2f01ab3594492118abb237ccdfd97
Jef Mathiot <jeff.mathiot@gmail.com>
2015-02-06 09:11:47 +0100

    Add support of GPG to the crypto module

--- lib/electric_sheep/crypto.rb
+++ lib/electric_sheep/crypto.rb
@@ -1,25 +1,162 @@
 require 'openssl'
 require 'base64'
+require 'fileutils'
 
 module ElectricSheep
   module Crypto
 
     class << self
 
+      def gpg
+        GPG
       end
 
     end
 
+    module GPG
 
+      module Encryptor
+        include Helpers::ShellSafe
 
+        BASE_COMMAND='gpg --batch'.freeze
 
         private
+        def exec(cmd)
+          result = Spawn.exec cmd
+          if result[:exit_status] != 0
+            raise "GPG command failed to complete \"#{cmd}\": #{result[:err]}"
           end
           result[:out]
         end
 
+        def keyid(keyfile)
+          keylist = exec "#{BASE_COMMAND} --with-colons --fixed-list-mode " +
+            "--keyid-format 0xlong #{keyfile}"
+          keylist.split(/\n+/).each do |line|
+            if line =~ /^(pub|sec):/
+              return line.split(':')[4]
+            end
+          end
+          raise "Unable to retrieve key info for #{keyfile}"
         end
 
+        def with_keyring(keyfile, &block)
+          # Using the block form to ensure the directory will be removed
+          Dir.mktmpdir do |homedir|
+            cmd = "#{BASE_COMMAND} --homedir #{homedir}"
+            import_key(cmd, keyfile)
+            yield cmd
+          end
+        end
+
+        def import_key(cmd, keyfile)
+          exec "#{cmd} --import #{keyfile}"
+        end
+
+        def with_content_file(text)
+          content = Tempfile.new('encryptor-content')
+          begin
+            content.write text
+            content.close
+            yield content.path
+          ensure
+            content.close
+            content.unlink
+          end
+        end
+
+        def command_options(action, keyfile)
+          " --no-version --#{action} --always-trust -r #{keyid(keyfile)}"
+        end
+
+        def ascii_armor(options)
+          options[:ascii] == true ? ' --armor' : ''
+        end
+
+        def wrap(action, keyfile, source, output = nil, &block)
+          with_keyring(keyfile) do |cmd|
+            cmd << yield if block_given?
+            cmd << command_options(action, keyfile)
+            wrapper = "cat #{source} | #{cmd}"
+            wrapper << " > #{output}" if output
+            exec wrapper
+          end
+        end
+
+      end
+
+      class FileEncryptor
+        include Encryptor
+
+        def encrypt(keyfile, source, output, options={})
+          perform(:encrypt, keyfile, source, output) do
+            ascii_armor(options)
+          end
+        end
+
+        def decrypt(keyfile, source, output, options={})
+          perform(:decrypt, keyfile, source, output)
+        end
+
+        private
+        def perform(action, keyfile, source, output, &block)
+          keyfile, source = File.expand_path(keyfile), File.expand_path(source)
+          File.expand_path(output).tap do |path|
+            wrap(action, keyfile, source, path, &block)
+          end
+        end
+
+      end
+
+      class StringEncryptor
+        include Encryptor
+
+        PGP_ARMOR_HEADER="-----BEGIN PGP MESSAGE-----".freeze
+        PGP_ARMOR_FOOTER="-----END PGP MESSAGE-----".freeze
+
+        def encrypt(keyfile, plain_text, options={})
+          output = perform(:encrypt, keyfile, plain_text) do
+            ascii_armor(options)
+          end
+          options[:compact] ? compact(output) : output
+        end
+
+        def decrypt(keyfile, cipher_text, options={})
+          cipher_text = expand(cipher_text)
+          perform(:decrypt, keyfile, cipher_text)
+        end
+
+        def perform(action, keyfile, input, &block)
+          keyfile = File.expand_path(keyfile)
+          output = nil
+          with_content_file(input) do |source|
+            output = wrap(action, keyfile, source, nil, &block)
+          end
+          output
+        end
+
+        private
+        def expand(cipher_text)
+          return cipher_text if cipher_text =~ /^#{PGP_ARMOR_HEADER}/
+          "#{PGP_ARMOR_HEADER}\n\n#{cipher_text}\n##{PGP_ARMOR_FOOTER}"
+        end
+
+        def compact(cipher_text)
+          return cipher_text unless cipher_text =~ /^#{PGP_ARMOR_HEADER}/
+          cipher_text.gsub(/-----(BEGIN|END) PGP MESSAGE-----/, '').
+            split(/\n+/).join
+        end
+
+      end
+
+      class << self
+
+        def file
+          FileEncryptor.new
+        end
+
+        def string
+          StringEncryptor.new
         end
 
       end
