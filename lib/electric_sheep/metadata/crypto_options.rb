7d94254be4b90cfcca1937d25c0614015f468eb7
Jef Mathiot <jeff.mathiot@gmail.com>
2015-02-06 08:51:32 +0100

    Add metadata to support encryption and decryption options

---
+++ lib/electric_sheep/metadata/crypto_options.rb
@@ -0,0 +1,24 @@
+module ElectricSheep
+  module Metadata
+
+    module CryptoOptions
+      extend ActiveSupport::Concern
+
+      included do
+        option :with, required: true
+      end
+
+    end
+
+    class EncryptOptions < Base
+      include CryptoOptions
+      # TODO Validate the provided key is a private key
+    end
+
+    class DecryptOptions < Base
+      include CryptoOptions
+      # TODO Validate the provided key is a public key
+    end
+
+  end
+end
---
+++ spec/electric_sheep/metadata/crypto_options_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe ElectricSheep::Metadata::CryptoOptions do
+  include Support::Options
+
+  OptionsKlazz = Class.new(ElectricSheep::Metadata::Base) do
+    include ElectricSheep::Metadata::CryptoOptions
+  end
+
+  describe OptionsKlazz do
+
+    it {
+      defines_options :with
+      requires :with
+    }
+
+  end
+end
+
+describe ElectricSheep::Metadata::EncryptOptions do
+end
+
+describe ElectricSheep::Metadata::DecryptOptions do
+end
