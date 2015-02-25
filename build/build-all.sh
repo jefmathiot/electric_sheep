469eec3b74866fd93abf9c27057558a5a8afc831
Jef Mathiot <jeff.mathiot@gmail.com>
2015-02-25 10:45:27 +0100

    Use the current version in the Dockerfile

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-cd vagrant
+BUILD_DIR=`pwd`
+
+ES_VERSION=$(ruby -r '../lib/electric_sheep/version.rb' -e "puts ElectricSheep::VERSION")
+
+cd $BUILD_DIR/vagrant
 vagrant up --provision && vagrant halt
 
-cd ..
-
-docker build --no-cache=true .
+cd $BUILD_DIR
+rm -f pkg/electric-sheep-docker.deb
+cp pkg/electric-sheep-ubuntu_${ES_VERSION}-1_amd64.deb pkg/electric-sheep-docker.deb
+docker build --no-cache -t servebox/electric_sheep .
+docker tag servebox/electric_sheep:latest servebox/electric_sheep:${ES_VERSION}
