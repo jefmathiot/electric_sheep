09000d53c0e5bea2bfea30f7758d6e7aab6ed0e3
Jef Mathiot <jeff.mathiot@gmail.com>
2015-03-22 19:17:01 +0100

    Add the missing DeleteSource module & fix the previous commit

---
+++ lib/electric_sheep/commands/delete_source.rb
@@ -0,0 +1,23 @@
+module ElectricSheep
+  module Commands
+
+    module DeleteSource
+      extend ActiveSupport::Concern
+
+      included do
+        option :delete_source
+      end
+
+      protected
+
+      def delete_source!(input_path)
+        if option(:delete_source)
+          shell.exec "rm -rf #{input_path}"
+          input.transient!
+        end
+      end
+
+    end
+
+  end
+end
